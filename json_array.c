fe686faf19bae8278bc5c4cfd9a0d385afa984b5
Stanislav Goriainov <goriainov@ispras.ru>
2023-08-26 15:14:27 +0300

    Finish writing JSON array.

--- json_array.c
+++ json_array.c
@@ -21,30 +21,52 @@ struct JsonArray *json_array_new() {
 }
 
 void json_array_delete(struct JsonArray *json_array) {
-    // TODO: poison struct data
     free(json_array->data);
+    json_array->data = NULL;
+    json_array->size = 0;
+    json_array->capacity = 0;
     free(json_array);
 }
 
 static void json_array_reserve(struct JsonArray *json_array, size_t capacity) {
     // TODO: check realloc for faulures
-    json_array->data = realloc(json_array->data, capacity * sizeof(struct JsonValue));
+    json_array->data =
+        realloc(json_array->data, capacity * sizeof(struct JsonValue));
     json_array->capacity = capacity;
 }
 
-void json_array_insert(struct JsonArray *json_array, struct JsonValue *value, size_t position) {
+void json_array_insert(
+    struct JsonArray *json_array,
+    struct JsonValue *value,
+    size_t position
+) {
     if (json_array->size >= json_array->capacity) {
-        json_array_reserve(json_array, json_array->capacity * JSON_ARRAY_GROW_RATE);
+        json_array_reserve(
+            json_array,
+            json_array->capacity * JSON_ARRAY_GROW_RATE
+        );
     }
 
     position = MIN(json_array->size, position);
-    memmove(&json_array->data[position + 1], &json_array->data[position], json_array->size - position);
-    memcpy(&json_array->data[json_array->size++], value, sizeof(struct JsonValue));
+    memmove(
+        &json_array->data[position + 1],
+        &json_array->data[position],
+        json_array->size - position
+    );
+    memcpy(
+        &json_array->data[json_array->size++],
+        value,
+        sizeof(struct JsonValue)
+    );
 }
 
 void json_array_erase(struct JsonArray *json_array, size_t position) {
     position = MIN(json_array->size, position);
-    memmove(&json_array->data[position], &json_array->data[position + 1], json_array->size - position);
+    memmove(
+        &json_array->data[position],
+        &json_array->data[position + 1],
+        json_array->size - (position + 1)
+    );
     json_array->size--;
 }
 
@@ -57,5 +79,11 @@ struct JsonValue *json_array_at(struct JsonArray *json_array, size_t position) {
 }
 
 void json_array_print(struct JsonArray *json_array, FILE *file) {
-    // TODO: create json_value_print() function
+    fprintf(file, "[\n");
+
+    for (size_t i = 0; i < json_array->size; i++) {
+        json_value_print(json_array_at(json_array, i), file);
+    }
+    
+    fprintf(file, "]\n");
 }
--- json_array.h
+++ json_array.h
@@ -9,18 +9,83 @@
 
 #define JSON_ARRAY_GROW_RATE 2
 
+/**
+ * @brief Represents JSON array
+ * 
+ */
 struct JsonArray {
     struct JsonValue *data;
     size_t size;
     size_t capacity;
 };
 
+/**
+ * @brief Allocates memory for a new JSON array
+ * 
+ * @return struct JsonArray* Pointer to the new JSON array
+ */
 struct JsonArray *json_array_new();
+
+/**
+ * @brief Deallocates memory for a given JSON array
+ * 
+ * @param json_array JSON array to delete
+ */
 void json_array_delete(struct JsonArray *json_array);
 
-void json_array_insert(struct JsonArray *json_array, struct JsonValue *value, size_t position);
+/**
+ * @brief Inserts a JSON value into a given JSON array
+ * 
+ * @param json_array JSON array to insert the value into
+ * @param value      JSON value to insert
+ * @param position   Position to insert to
+ */
+void json_array_insert(
+    struct JsonArray *json_array,
+    struct JsonValue *value,
+    size_t position
+);
+
+/**
+ * @brief Erases a JSON value from a given JSON array
+ * 
+ * @param json_array JSON array to erase the value from
+ * @param position   Position to erase from
+ */
 void json_array_erase(struct JsonArray *json_array, size_t position);
+
+/**
+ * @brief Provides a JSON value at a given position
+ * 
+ * @param json_array JSON array to get the value from
+ * @param position   Position to get the value from
+ * @return struct JsonValue* Pointer to a corresponding JSON value
+ * or NULL in case of failure
+ */
 struct JsonValue *json_array_at(struct JsonArray *json_array, size_t position);
+
+/**
+ * @brief Provides the size of a given JSON array
+ * 
+ * @param json_array JSON array to get the size of
+ * @return size_t    The size of a given JSON array
+ */
+size_t json_array_size(struct JsonArray *json_array);
+
+/**
+ * @brief Provides the capacity of a given JSON array
+ * 
+ * @param json_array JSON array to get the capacity of
+ * @return size_t    The capacity of a given JSON array
+ */
+size_t json_array_capacity(struct JsonArray *json_array);
+
+/**
+ * @brief Prints a JSON array in a JSON format
+ * 
+ * @param json_array JSON array to print
+ * @param file       File to print the JSON array into
+ */
 void json_array_print(struct JsonArray *json_array, FILE *file);
 
 #endif // JSON_ARRAY_H
