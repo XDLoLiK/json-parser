cc913f806cd039f816917bbadbd2b389008f2bee
Stanislav Goriainov <goriainov@ispras.ru>
2023-08-26 15:15:22 +0300

    Finish writing JSON value.

--- json_value.c
+++ json_value.c
@@ -2,13 +2,19 @@
 #include "json_object.h"
 #include "json_value.h"
 
-struct JsonValue *json_value_new(enum JsonValueTypes json_value_type) {
+const char *LITERAL_NAMES[] = {
+    "false",
+    "null",
+    "true",
+};
+
+struct JsonValue *json_value_new() {
     struct JsonValue *json_value = calloc(1, sizeof(struct JsonValue));
     return json_value;
 }
 
 void json_value_delete(struct JsonValue *json_value) {
-    switch (json_value.value_type) {
+    switch (json_value->type) {
         case Object: {
             json_object_delete(json_value->object);
             break;
@@ -21,7 +27,42 @@ void json_value_delete(struct JsonValue *json_value) {
             free(json_value->string);
             break;
         }
+        default: {
+            break;
+        }
     }
 
     free(json_value);
+}
+
+enum JsonValueTypes json_value_type(struct JsonValue *json_value) {
+    return json_value->type;
+}
+
+void json_value_print(struct JsonValue *json_value, FILE *file) {
+    switch (json_value->type) {
+        case Object: {
+            json_object_print(json_value->object, file);
+            break;
+        }
+        case Array: {
+            json_array_print(json_value->array, file);
+            break;
+        }
+        case String: {
+            fprintf(file, "\"%s\"\n", json_value->string);
+            break;
+        }
+        case Number: {
+            fprintf(file, "%lf\n", json_value->number);
+            break;
+        }
+        case LiteralName: {
+            fprintf(file, "%s\n", LITERAL_NAMES[json_value->literal_name]);
+            break;
+        }
+        default: {
+            break;
+        }
+    }
 }
--- json_value.h
+++ json_value.h
@@ -1,13 +1,26 @@
 #ifndef JSON_VALUE_H
 #define JSON_VALUE_H
 
+#include <stddef.h>
+#include <stdio.h>
+
+extern const char *LITERAL_NAMES[];
+
+/**
+ * @brief 
+ * 
+ */
 enum JsonLiteralNames {
-    False, // "false"
-    Null, // "null"
-    True, // "true"
+    False,
+    Null,
+    True,
     LiteralNameMax,
 };
 
+/**
+ * @brief 
+ * 
+ */
 enum JsonValueTypes {
     Object,
     Array,
@@ -17,18 +30,49 @@ enum JsonValueTypes {
     ValueTypeMax,
 };
 
+/**
+ * @brief 
+ * 
+ */
 struct JsonValue {
-    enum JsonValueTypes value_type;
+    enum JsonValueTypes type;
     union {
         struct JsonObject *object;
         struct JsonArray *array;
         double number;
-        const char *string;
+        char *string;
         enum JsonLiteralNames literal_name;
     };
 };
 
+/**
+ * @brief 
+ * 
+ * @return struct JsonValue* 
+ */
 struct JsonValue *json_value_new();
+
+/**
+ * @brief 
+ * 
+ * @param json_value 
+ */
 void json_value_delete(struct JsonValue *json_value);
+
+/**
+ * @brief 
+ * 
+ * @param json_value 
+ * @return enum JsonValueTypes 
+ */
+enum JsonValueTypes json_value_type(struct JsonValue *json_value);
+
+/**
+ * @brief 
+ * 
+ * @param json_value 
+ * @param file 
+ */
+void json_value_print(struct JsonValue *json_value, FILE *file);
 
 #endif // JSON_VALUE_H
